aa61db332a4e6c2469a808684394dd63e26d9d5f
Alexander Balandin <my@maggit.ru>
2019-12-15 17:53:56 +0300

    Add shader sketches

--- src/s_6-shader.js
+++ src/s_6-shader.js
@@ -13,21 +13,11 @@ const frag = glsl`
   precision highp float;
 
   uniform float time;
-  uniform float aspect;
   varying vec2 vUv;
 
   void main () {
-    vec3 colorA = vec3(0.1, 0.1, 0.5);
-    vec3 colorB = vec3(0.0, 0.1, 0.4);
-
-    vec2 center = vUv - 0.5;
-    center.x *= aspect;
-    float dist = length(center);
-
-    float alpha = smoothstep(cos(time * 0.2) * 0.1 + 0.1, 0.2, dist);
-
-    vec3 color = mix(colorA, colorB, vUv.x + vUv.y);
-    gl_FragColor = vec4(color, alpha);
+    vec3 color = 0.5 + 0.5 * cos(time + vUv.xyx + vec3(0.0, 2.0, 4.0));
+    gl_FragColor = vec4(color, 1.0);
   }
 `
 
@@ -35,7 +25,6 @@ const frag = glsl`
 const sketch = ({ gl }) => {
   // Create the shader and return it
   return createShader({
-    clearColor: 'black',
     // Pass along WebGL context
     gl,
     // Specify fragment and/or vertex shader strings
@@ -43,8 +32,7 @@ const sketch = ({ gl }) => {
     // Specify additional uniforms to pass down to the shaders
     uniforms: {
       // Expose props from canvas-sketch
-      time: ({ time }) => time,
-      aspect: ({ width, height }) => width / height
+      time: ({ time }) => time
     }
   })
 }
--- src/s_7-disco.js
+++ src/s_7-disco.js
@@ -17,16 +17,16 @@ const frag = glsl`
   varying vec2 vUv;
 
   void main () {
-    vec3 colorA = vec3(0.1, 0.1, 0.5);
+    vec3 colorA = vec3(0.7, 0.0, 0.5);
     vec3 colorB = vec3(0.0, 0.1, 0.4);
 
     vec2 center = vUv - 0.5;
     center.x *= aspect;
     float dist = length(center);
 
-    float alpha = smoothstep(cos(time * 0.2) * 0.1 + 0.1, 0.2, dist);
+    float alpha = smoothstep(cos(time * 0.2) * 0.1 + 0.1, sin(time * 1.5 + sin(time * 20.0)) * 0.5 + 0.2, dist);
 
-    vec3 color = mix(colorA, colorB, vUv.x + vUv.y);
+    vec3 color = mix(colorA, colorB, vUv.x + vUv.y + sin(time));
     gl_FragColor = vec4(color, alpha);
   }
 `
